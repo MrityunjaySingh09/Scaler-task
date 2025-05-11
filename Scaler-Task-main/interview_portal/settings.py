780f8099f70b98dab3d1fe3f095dbb70f77dac19
MrityunjaySingh09 <137044290+MrityunjaySingh09@users.noreply.github.com>
2025-05-11 20:10:22 +0530

    Updated

--- Scaler-Task-main/interview_portal/settings.py
+++ Scaler-Task-main/interview_portal/settings.py
@@ -24,6 +24,7 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
